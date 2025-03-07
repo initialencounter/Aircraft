60206ebb7fe3ecfe1d1fa80ca1f158cc0ef34459
initialencounter <2911583893@qq.com>
2025-03-08 00:02:48 +0800

    Show json that cannot be parsed

--- src/views/PdfParse.vue
+++ src/views/PdfParse.vue
@@ -8,44 +8,61 @@ import PdfData from "../components/PdfData.vue";
 import { DataModel } from "../types";
 
 const parseResult = ref<DataModel>({
-   "appraiserCName": "",
-   "appraiserEName": "",
-   "manufacturerCName": "",
-   "manufacturerEName": "",
-   "itemCName": "",
-   "itemEName": "",
-   "color": "",
-   "shape": "",
-   "size": "",
-   "model": "",
-   "brands": "",
-   "btyCount": "",
-   "netWeight": "",
-   "inspectionItem2Text1": "",
-   "inspectionItem2Text2": "",
-   "inspectionItem3Text1": "",
-    "market": ""
- });
+  appraiserCName: "",
+  appraiserEName: "",
+  manufacturerCName: "",
+  manufacturerEName: "",
+  itemCName: "",
+  itemEName: "",
+  color: "",
+  shape: "",
+  size: "",
+  model: "",
+  brands: "",
+  btyCount: "",
+  netWeight: "",
+  inspectionItem2Text1: "",
+  inspectionItem2Text2: "",
+  inspectionItem3Text1: "",
+  market: "",
+});
 
+const showRawText = ref(true);
+const rawText = ref("");
 ipcManager.invoke("switch_drag_to_blake2", { value: false });
-ipcManager.on("pdf_reader_result", (data: Event<DataModel>): void => {
-  parseResult.value = data.payload
+ipcManager.on("pdf_reader_result", (data: Event<string>): void => {
+  try {
+    showRawText.value = false;
+    parseResult.value = JSON.parse(data.payload) as DataModel;
+  } catch (e) {
+    showRawText.value = true;
+    rawText.value = data.payload;
+  }
 });
 
 document.oncontextmenu = function () {
   return false;
 };
-
 </script>
 
 <template>
   <!-- 头部 -->
-  <h1 class="noSelectTitle" data-tauri-drag-region style="font-size: 24px">
-  </h1>
+  <h1 class="noSelectTitle" data-tauri-drag-region style="font-size: 24px"></h1>
   <!-- 内容区 -->
   <br />
-  <PdfData :data="parseResult" />
+  <div v-if="!showRawText">
+    <PdfData :data="parseResult" />
+  </div>
+  <div v-else>
+    <el-input
+      class="rawText"
+      v-model="rawText"
+      style="width: 100%"
+      :rows="15"
+      type="textarea"
+      placeholder="请拖拽报告到此次区域"
+    />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
