eff275bcf7be8bdf8f67de619c01c3d6fb433bac
initialencounter <2911583893@qq.com>
2025-04-28 22:27:13 +0800

    feat: 添加托盘菜单项，提供帮助、关于和检查更新功能

--- electron/main/service/tray.ts
+++ electron/main/service/tray.ts
@@ -1,8 +1,9 @@
-import { Tray, Menu } from 'electron'
+import { Tray, Menu, BrowserWindow } from 'electron'
 import path from 'path'
 import { Context, Service } from 'cordis'
 import type { } from '../service/app'
 import type { } from '../service/win'
+import type { } from '@cordisjs/plugin-http'
 
 declare module 'cordis' {
   interface Context {
@@ -11,7 +12,7 @@ declare module 'cordis' {
 }
 
 class CustomTray extends Service {
-  static inject = ['app', 'win']
+  static inject = ['app', 'win', 'http']
   tray: Tray | null
   constructor(ctx: Context) {
     super(ctx, 'tray')
@@ -30,12 +31,52 @@ class CustomTray extends Service {
       }
     })
     const contextMenu = Menu.buildFromTemplate([
+      //   let help_ = MenuItemBuilder::new("帮助(H)").id("help").build(app).unwrap();
+      // let quit = MenuItemBuilder::new("退出(X)").id("quit").build(app).unwrap();
+      // let hide = MenuItemBuilder::new("隐藏(H)").id("hide").build(app).unwrap();
+      // let about = MenuItemBuilder::new("关于(A)").id("about").build(app).unwrap();
+      // let update = MenuItemBuilder::new("检查更新(U)").id("update").build(app).unwrap();
+      // let restart_ = MenuItemBuilder::new("重启(R)").id("restart").build(app).unwrap();
       {
-        label: '退出',
+        label: '帮助(H)',
+        click: () => {
+          new BrowserWindow({
+            icon: path.join(this.ctx.app.VITE_PUBLIC, 'favicon.ico'),
+            show: true,
+          }).loadURL('https://github.com/initialencounter/Aircraft?tab=readme-ov-file#使用帮助')
+        }
+      },
+      {
+        label: '退出(X)',
         click: () => {
           this.ctx.app.app.quit()
         }
-      }
+      },
+      {
+        label: '隐藏(H)',
+        click: () => {
+          this.ctx.win.win?.hide()
+        }
+      },
+
+      {
+        label: '关于(A)',
+        click: () => {
+          new BrowserWindow({
+            icon: path.join(this.ctx.app.VITE_PUBLIC, 'favicon.ico'),
+            show: true,
+          }).loadURL('https://github.com/initialencounter/Aircraft')
+        }
+      },
+      {
+        label: '检查更新(U)',
+        click: async () => {
+          new BrowserWindow({
+            icon: path.join(this.ctx.app.VITE_PUBLIC, 'favicon.ico'),
+            show: true,
+          }).loadURL('https://github.com/initialencounter/Aircraft/releases/latest')
+        }
+      },
     ])
     this.tray.setContextMenu(contextMenu)
   }
