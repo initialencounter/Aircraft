5599bab07dbc602fb5d84ebbef8d03046dab4faf
initialencounter <2911583893@qq.com>
2025-05-09 19:50:18 +0800

    feat(pdf-parse): 添加相对定位容器并调整遮罩层样式

--- packages/renderer/src/views/PdfParse.vue
+++ packages/renderer/src/views/PdfParse.vue
@@ -90,59 +90,69 @@ function fileToBase64(file: File): Promise<string> {
 </script>
 
 <template>
-  <!-- 头部 -->
-  <h1 class="noSelectTitle" style="font-size: 24px"></h1>
-  <!-- 内容区 -->
-  <FileDropzone
-    accept=".pdf,.docx"
-    :multiple="true"
-    :maxSize="20"
-    :maxFiles="5"
-    @files-change="handleFilesChange"
-    @file-select="handleFileSelect"
-    @file-remove="handleFileRemove"
-    @parse-report="handleParseReport"
-  />
-  <el-radio-group v-model="labelPosition">
-    <el-radio value="result">验证结果</el-radio>
-    <el-radio value="summary">概要</el-radio>
-    <el-radio value="UN38.3">UN38.3报告</el-radio>
-  </el-radio-group>
-  <br />
-  <k-markdown
-    v-if="labelPosition === 'result'"
-    :source="'- ' + verifyResult.join('\n- ')"
-  ></k-markdown>
-  <summaryTable
-    v-if="labelPosition === 'summary'"
-    :data="parseResult"
-  ></summaryTable>
-  <summaryTable
-    v-if="labelPosition === 'UN38.3'"
-    :data="llmResult"
-    :isUN38="true"
-  ></summaryTable>
-  <!-- 添加遮罩层 -->
-  <div class="loading-mask" v-if="loading" @dblclick="loading = false">
-    <div class="loading-content">
-      <el-icon class="loading-icon"><Loading /></el-icon>
-      <span>正在解析文件，请稍候...<br />双击关闭遮罩</span>
+  <!-- 添加一个相对定位的容器包裹整个组件内容 -->
+  <div class="pdf-parse-container">
+    <!-- 头部 -->
+    <h1 class="noSelectTitle" style="font-size: 24px"></h1>
+    <!-- 内容区 -->
+    <FileDropzone
+      accept=".pdf,.docx"
+      :multiple="true"
+      :maxSize="20"
+      :maxFiles="5"
+      @files-change="handleFilesChange"
+      @file-select="handleFileSelect"
+      @file-remove="handleFileRemove"
+      @parse-report="handleParseReport"
+    />
+    <el-radio-group v-model="labelPosition">
+      <el-radio value="result">验证结果</el-radio>
+      <el-radio value="summary">概要</el-radio>
+      <el-radio value="UN38.3">UN38.3报告</el-radio>
+    </el-radio-group>
+    <br />
+    <k-markdown
+      v-if="labelPosition === 'result'"
+      :source="'- ' + verifyResult.join('\n- ')"
+    ></k-markdown>
+    <summaryTable
+      v-if="labelPosition === 'summary'"
+      :data="parseResult"
+    ></summaryTable>
+    <summaryTable
+      v-if="labelPosition === 'UN38.3'"
+      :data="llmResult"
+      :isUN38="true"
+    ></summaryTable>
+
+    <!-- 添加遮罩层 - 现在相对于容器定位 -->
+    <div class="loading-mask" v-if="loading" @dblclick="loading = false">
+      <div class="loading-content">
+        <el-icon class="loading-icon"><Loading /></el-icon>
+        <span>正在解析文件，请稍候...<br />双击关闭遮罩</span>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.pdf-parse-container {
+  position: relative; /* 设置为相对定位，作为遮罩的定位参考 */
+  width: 100%;
+  height: 100%;
+}
+
 .loading-mask {
-  position: fixed;
+  position: absolute; /* 改为绝对定位，相对于.pdf-parse-container */
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: 180%;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 9999;
+  z-index: 10; /* 降低z-index，只要能覆盖当前组件即可 */
 }
 
 .loading-content {
@@ -151,6 +161,7 @@ function fileToBase64(file: File): Promise<string> {
   display: flex;
   flex-direction: column;
   align-items: center;
+  color: white; /* 确保文字在深色背景上可见 */
 }
 
 .loading-icon {
