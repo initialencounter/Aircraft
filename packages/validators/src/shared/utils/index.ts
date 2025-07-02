208b29b965341b2cc03680599fb0b30225093abc
initialencounter <2911583893@qq.com>
2025-07-03 02:07:18 +0800

    fix: match more unit (close #4)

--- packages/validators/src/shared/utils/index.ts
+++ packages/validators/src/shared/utils/index.ts
@@ -16,38 +16,44 @@ import {
 } from './matchDevice'
 
 function matchWattHour(projectName: string) {
-  const matches = [...projectName.matchAll(/\s(\d+\.?\d+)[Kk]?[Ww][Hh]/g)]
+  const matches = [...projectName.matchAll(/\s(\d+\.?\d*)\s*[MmKk]?[Ww][Hh]/g)]
   const results = matches.map((match) => match[1])
   const rowText = matches.map((match) => match[0])[results.length - 1]
   let wattHour = Number(results[results.length - 1])
   if (!results.length) return 0
   if (isNaN(wattHour)) return 0
   if (rowText.toLowerCase().includes('kwh')) wattHour *= 1000
+  if (rowText.toLowerCase().includes('mwh')) wattHour /= 1000
   return wattHour
 }
 
 function matchVoltage(projectName: string) {
-  const matches = [...projectName.matchAll(/(\d+\.?\d*)[Vv]/g)]
+  const matches = [...projectName.matchAll(/(\d+\.?\d*)\s*[MmKk]?[Vv]/g)]
   const results = matches.map((match) => match[1])
-  const voltage = Number(results[results.length - 1])
+  const rowText = matches.map((match) => match[0])[results.length - 1]
+  let voltage = Number(results[results.length - 1])
   if (!results.length) return 0
   if (isNaN(voltage)) return 0
+  if (rowText.toLowerCase().includes('mv')) voltage /= 1000
+  if (rowText.toLowerCase().includes('kv')) voltage *= 1000
   return voltage
 }
 
 function matchCapacity(projectName: string) {
-  const matches = [...projectName.matchAll(/(\d+\.?\d*)[Mm]?[Aa][Hh]/g)]
+  const matches = [...projectName.matchAll(/(\d+\.?\d*)\s*[MmKk]?[Aa][Hh]/g)]
   const results = matches.map((match) => match[1])
   const rowText = matches.map((match) => match[0])[results.length - 1]
   let result = Number(results[results.length - 1])
   if (!results.length) return 0
   if (isNaN(result)) return 0
-  if (!rowText.toLowerCase().includes('mah')) result *= 1000
+  if (rowText.toLowerCase().includes('kah')) {// kAh
+    result *= 1000 * 1000
+  } else if (!rowText.toLowerCase().includes('mah')) result *= 1000
   return result
 }
 
 function matchBatteryWeight(describe: string) {
-  const matches = [...describe.matchAll(/为(\d+\.?\d*)[Kk]?g?/g)]
+  const matches = [...describe.matchAll(/为(\d+\.?\d*)\s*[Kk]?g?/g)]
   const results = matches.map((match) => match[1])
   const rowText = matches.map((match) => match[0])[0]
   let weight = Number(results[0])
