4cd1c46c7725ba072358422b1113db28ed37ab68
initialencounter <2911583893@qq.com>
2025-09-06 00:36:01 +0800

    drop dotenv

--- packages/wxt/web-ext.config.ts
+++ packages/wxt/web-ext.config.ts
@@ -1,13 +1,8 @@
 import { resolve } from 'node:path'
 import { defineWebExtConfig } from 'wxt'
-import dotenv from 'dotenv'
-dotenv.config()
-
-const startUrls = process.env.START_URLS?.split(',') || ['https://wxt.dev']
 
 export default defineWebExtConfig({
   // On Windows, the path must be absolute
-  startUrls: startUrls,
   chromiumProfile: resolve('./chrome-data'),
   keepProfileChanges: true,
   binaries: {
