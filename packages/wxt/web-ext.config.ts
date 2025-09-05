1bb59794729d1d82e45860e62b07c2f603a9f492
initialencounter <2911583893@qq.com>
2025-09-05 23:19:13 +0800

    chore

--- packages/wxt/web-ext.config.ts
+++ packages/wxt/web-ext.config.ts
@@ -10,4 +10,9 @@ export default defineWebExtConfig({
   startUrls: startUrls,
   chromiumProfile: resolve('./chrome-data'),
   keepProfileChanges: true,
+  binaries: {
+    chrome: 'C:\\Users\\29115\\AppData\\Local\\360ChromeX\\Chrome\\Application\\360ChromeX.exe',
+    edge: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+    firefox: 'C:\\Program Files\\Mozilla Firefox\\firefox.exe',
+  },
 })
