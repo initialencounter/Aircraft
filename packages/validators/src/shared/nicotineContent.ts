f34732f94e41c1a1f9835b2613cba00ff4207d9d
initialencounter <2911583893@qq.com>
2025-09-17 13:35:30 +0800

    fix(validators): Nicotine verify
    Signed-off-by: initialencounter <2911583893@qq.com>

--- packages/validators/src/shared/nicotineContent.ts
+++ packages/validators/src/shared/nicotineContent.ts
@@ -1,17 +1,23 @@
 import type { CheckResult } from './types'
 
-export function checkNicotineContent(otherDescribeCAddition: string): CheckResult[] {
-  const hasNicotine = otherDescribeCAddition.includes('尼古丁含量') ||
-    otherDescribeCAddition.includes('尼古丁盐含量');
-  const hasFivePercent = otherDescribeCAddition.includes('5%');
-  const hasMassVolume = otherDescribeCAddition.includes('质量体积分数');
+export function checkNicotineContent(
+  otherDescribeCAddition: string
+): CheckResult[] {
+  const hasNicotine =
+    otherDescribeCAddition.includes('尼古丁含量') ||
+    otherDescribeCAddition.includes('尼古丁盐含量')
+  const hasFivePercent = otherDescribeCAddition.includes('5%')
+  const hasPointFivePercent = otherDescribeCAddition.includes('.5%')
+  const hasMassVolume = otherDescribeCAddition.includes('质量体积分数')
 
-  if (hasNicotine && hasFivePercent && !hasMassVolume) {
-    return [{
-      ok: false,
-      result: '尼古丁含量5%缺少质量体积分数'
-    }];
+  if (hasNicotine && hasFivePercent && !hasMassVolume && !hasPointFivePercent) {
+    return [
+      {
+        ok: false,
+        result: '尼古丁含量5%缺少质量体积分数',
+      },
+    ]
   }
 
-  return [];
+  return []
 }
