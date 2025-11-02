a8d85d4c02186d5b2f7fcf9ec5e6094939b08167
initialencounter <2911583893@qq.com>
2025-11-02 20:59:40 +0800

    migrate to https://github.com/initialencounter/clipkeeper

--- clipboard_persistence/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(windows)]
-pub mod windows_clipboard;
-
-#[cfg(windows)]
-pub use windows_clipboard::{
-    get_windows_clipboard_snapshot, restore_windows_clipboard_snapshot,
-    WindowsClipboardSnapshot, WindowsClipboardFormat,
-};
