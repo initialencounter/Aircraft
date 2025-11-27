56cca4623c2de4e7905295ba356f3b7036ceff2c
initialencounter <2911583893@qq.com>
2025-11-27 13:53:57 +0800

    release(wxt): wxt-v3.0.4

--- packages/wxt/wxt.config.ts
+++ packages/wxt/wxt.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   entrypointsDir: './entrypoints',
   manifest: {
     name: 'lims',
-    version: '3.0.3',
+    version: '3.0.4',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     web_accessible_resources: [
       {
