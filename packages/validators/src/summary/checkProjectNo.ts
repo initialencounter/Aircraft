d891f0a9150a88c65eed0297a288273e0b402d11
initialencounter <2911583893@qq.com>
2025-12-21 10:52:33 +0800

    fix(validators): summary project no result

--- packages/validators/src/summary/checkProjectNo.ts
+++ packages/validators/src/summary/checkProjectNo.ts
@@ -5,7 +5,7 @@ export function checkProjectNo(
   summaryProjectNo: string
 ): CheckResult[] {
   if (formProjectNo !== summaryProjectNo.trim()) {
-    return [{ ok: false, result: '项目编号不一致' }]
+    return [{ ok: false, result: '概要项目编号不一致' }]
   }
   return []
 }
