10679f4e32e0e0bf20beb2d19261f35a7d1cb7a9
initialencounter <2911583893@qq.com>
2026-02-27 17:27:20 +0800

    release(wxt): wxt-3.2.6

--- packages/wxt/docs/changelog.md
+++ packages/wxt/docs/changelog.md
@@ -1,5 +1,11 @@
 # 更新日志
 
+## [v3.2.6] - 2026-02-27
+
+### 修复
+
+- 空运单电芯钠离子电池类型验证([5fc98a0](https://github.com/initialencounter/Aircraft/commit/5fc98a039b741aa4828984006fa195d60d37c2d2))
+
 ## [v3.2.5] - 2026-02-27
 
 ### 新增
--- packages/wxt/wxt.config.ts
+++ packages/wxt/wxt.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   entrypointsDir: './entrypoints',
   manifest: {
     name: 'lims',
-    version: '3.2.5',
+    version: '3.2.6',
     description: 'Automates form validation and data entry for battery inspection forms.',
     web_accessible_resources: [
       {
