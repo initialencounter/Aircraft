e3d677746619f4fe67f238688337e6cd31acc860
initialencounter <2911583893@qq.com>
2026-03-03 11:06:53 +0800

    fix(validators): improve g unit checks in descriptionFormat function

--- packages/validators/src/lithium/shared/descriptionFormat.ts
+++ packages/validators/src/lithium/shared/descriptionFormat.ts
@@ -1,13 +1,13 @@
 import type { CheckResult } from './types'
 
 export function descriptionFormat(otherDescribeCAddition: string): CheckResult[] {
-  if (otherDescribeCAddition.includes('gg。')) {
+  if (otherDescribeCAddition.includes('gg。') || otherDescribeCAddition.includes('gg，')) {
     return [{
       ok: false,
       result: '电池质量单位中存在两个g'
     }]
   }
-  if (!otherDescribeCAddition.includes('g。')) {
+  if (!otherDescribeCAddition.includes('g。') && !otherDescribeCAddition.includes('g，')) {
     return [{
       ok: false,
       result: '电池质量单位中缺少g'
