8cae1c3a401689c57572a091b2479384aa78d399
initialencounter <2911583893@qq.com>
2026-03-03 09:15:26 +0800

    feat(validators): add lithium T8 check
    Signed-off-by: initialencounter <2911583893@qq.com>

--- packages/validators/src/summary/checkT8.ts
+++ packages/validators/src/summary/checkT8.ts
@@ -4,13 +4,24 @@ export function checkT8(
   isSodium: boolean,
   summaryTest8: string,
 ): CheckResult[] {
-  if (summaryTest8.includes('通过') && isSodium) {
-    return [
-      {
-        ok: false,
-        result: `钠离子电池, 概要T8测试结果应为不适用`,
-      },
-    ]
+  if (isSodium) {
+    if (summaryTest8.includes('通过')) {
+      return [
+        {
+          ok: false,
+          result: `钠离子电池, 概要T8测试结果应为不适用`,
+        },
+      ]
+    }
+  } else {
+    if (!summaryTest8.includes('通过')) {
+      return [
+        {
+          ok: false,
+          result: `非钠离子电池, 概要T8测试结果应为通过`,
+        },
+      ]
+    }
   }
   return []
 }
