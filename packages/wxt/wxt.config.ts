852c6ee29a8e43647779199ad3f94653a0911e49
initialencounter <2911583893@qq.com>
2026-03-04 09:13:26 +0800

    release(wxt): wxt-3.4.1

--- packages/wxt/wxt.config.ts
+++ packages/wxt/wxt.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   entrypointsDir: './entrypoints',
   manifest: {
     name: 'lims',
-    version: '3.4.0',
+    version: '3.4.1',
     description: 'Automates form validation and data entry for battery inspection forms.',
     web_accessible_resources: [
       {
