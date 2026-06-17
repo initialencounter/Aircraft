028f7ccce47525cb2fd30d92ab17c37bd1cf2427
initialencounter <2911583893@qq.com>
2026-06-17 09:22:09 +0800

    fix: 简化错误标记元素的处理逻辑，直接操作元素而非其父元素

--- packages/wxt/entrypoints/modules/ui/markError.ts
+++ packages/wxt/entrypoints/modules/ui/markError.ts
@@ -2,18 +2,16 @@ export function markErrorElement(selector: string, message: string[]) {
   if (!selector) return
   const element = document.querySelector(selector) as HTMLInputElement | null
   if (!element) return
-  let targetElement: HTMLElement = element
-  if (element.type === 'radio' || element.type === 'hidden' || element.type === 'checkbox') {
-    targetElement = element.parentElement as HTMLElement
-    if (element.name === 'remarks') {
-      targetElement = element.parentElement?.parentElement as HTMLElement
-    }
-  }
-  targetElement.style.backgroundColor = message.length ? '#FF6347' : ''
-  targetElement.setAttribute('title', message.length ? message.join(';\n\n') : "")
+  element.style.backgroundColor = message.length ? '#FF6347' : ''
+  element.setAttribute('title', message.length ? message.join(';\n\n') : "")
 }
 
 export function clearError() {
+  const AttachmentElement = document.querySelector('#openDocumentsBtn0 > span > span.l-btn-text') as HTMLInputElement | null
+  if (AttachmentElement) {
+    AttachmentElement.style.backgroundColor = ''
+    AttachmentElement.setAttribute('title', '')
+  }
   const elements = document.querySelectorAll('[name]') as NodeListOf<HTMLInputElement>
   elements.forEach(element => {
     if (!element) return
